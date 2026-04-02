e4d84de67da5ab4168e44343ae51c1a6236aeb65
2smarthrm <2smarthrm@gmail.com>
2026-04-02 12:10:32 +0100

    Update index.js

--- index.js
+++ index.js
@@ -94,7 +94,7 @@ app.post("/sendemailaddressmessagetocompany", async (req, res) => {
   try {
     const { nif, name, company, sector, email, phone, employeesnumber, message } = req.body;
 
-    if (!name || !company || !phone || !employeesnumber || !message) {
+    if (!name || !company || !phone || !employeesnumber ) {
       return res.status(400).json({ success: false, error: "All fields are required!" });
     }
 
